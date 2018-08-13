326cd8c5dea021dcbedba5707c07c1c7a7953930
dlb <dlb96@qq.com>
2018-08-13 11:38:55 +0800

    123

--- vue/src/Snap.vue
+++ vue/src/Snap.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="snap">
-      <div id="cebian">
+      <sidebar id="cebian">
         <div id="self">
           <img src="../src/assets/head.jpg" alt="">
           <span>Snap</span>
         </div>
         <div v-for="skill in skills" v-bind:key="skill.index">
           <span>{{skill.name}}</span>
-          <span>{{skill.begintime}}至今</span>
+          <span>{{skill.begintime}}-至今</span>
+          <progress v-bind:value="skill.degree" max="100"></progress>
         </div>
-      </div>
+      </sidebar>
       <div>
-        <button v-on:click="addBlog()">添加博客</button>
+
       </div>
   </div>
 </template>
@@ -22,11 +23,18 @@ export default {
       skills: [
         {
           name: '前端开发',
-          begintime: '2017'
+          begintime: '2017',
+          degree: '50'
         },
         {
           name: 'c/cpp',
-          begintime: '2015'
+          begintime: '2015',
+          degree: '44'
+        },
+        {
+          name: 'vue',
+          begintime: '2018',
+          degree: '33'
         }
       ]
     }
@@ -54,5 +62,19 @@ export default {
     margin:20% 20% 0% 20%;
     width:60%;
     height:60%;
+  }
+
+  progress
+  {
+    width: 168px;
+    height: 5px;
+  }
+  progress::-webkit-progress-bar
+  {
+    background-color:#d7d7d7;
+  }
+  progress::-webkit-progress-value
+  {
+    background-color:orange;
   }
 </style>
