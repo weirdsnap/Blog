f12125fb803688ee77e4d8570d8ea5db3b0bd209
dlb <dlb96@qq.com>
2018-08-17 22:06:11 +0800

    test modified

--- entities/entities_test.go
+++ entities/entities_test.go
@@ -5,8 +5,11 @@ import (
 )
 
 func Test_init(t *testing.T) {
-	a := NewArticleInfo(ArticleInfo{AID: 123, Title: "asd", Class: "asd", Content: "asd"})
-	if e := ArticleInfoService.Save(a); e != nil {
+	init()
+	// a := NewArticleInfo(ArticleInfo{AID: 123, Title: "asd", Class: "asd", Content: "asd"})
+	
+	// if e := ArticleInfoService.Save(a); e != nil {
+	if e := ArticleInfoService.FindByTitle("asd"); e != nil {
 		t.Error("save函数测试没通过")
 	} else {
 		t.Log("save函数测试通过了")
